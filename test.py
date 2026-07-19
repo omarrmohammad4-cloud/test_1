026e94012ec6ec94d5d35bfaa2810fb8a6511d06
Roaa645 <roaa.abdelhakim@ejust.edu.eg>
2026-07-19 15:01:01 +0300

    Add workspace files

--- test.py
+++ test.py
@@ -1,16 +1,33 @@
 from langchain_ollama import ChatOllama
 
-# Load the model
 llm = ChatOllama(model="qwen3:8b")
+
+system_prompt = """
+You are an AI Assistant for an e-commerce platform.
+
+Your responsibilities are:
+- Answer merchant questions.
+- Generate professional product descriptions.
+- Recommend products.
+- Explain business information.
+- Always respond professionally.
+"""
 
 while True:
     question = input("You: ")
 
     if question.lower() == "exit":
-        print("Goodbye!")
         break
 
-    response = llm.invoke(question)
+    prompt = f"""
+{system_prompt}
 
-    print("\nAI:", response.content)
-    print("-" * 50)
+User Question:
+{question}
+"""
+
+    response = llm.invoke(prompt)
+
+    print("\nAI:")
+    print(response.content)
+    print("-" * 60)
